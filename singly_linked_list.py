e2beff013296437addcb5039c7b2ad9e2eb6e9b3
PW277 <66410568+PW277@users.noreply.github.com>
2025-02-24 18:10:22 +0000

    Initial code added. Basic ll structure with print

---
+++ singly_linked_list.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=' -> ')
+            current_node = current_node.next
+        print("None")
+
+ll = SinglyLinkedList()
+
+ll.append(1)
+ll.append(2)
+ll.append(3)
+
+ll.print_list()
+
+#ask about more operations in a sec
